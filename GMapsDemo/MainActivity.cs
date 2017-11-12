0ed5730d88abd12f16651c5e3775bbafdb74416b
jason-weaver <jason.weaver@utexas.edu>
2017-11-12 14:58:36 -0600

    Add project files.

---
+++ GMapsDemo/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Maps;
+
+namespace GMapsDemo
+{
+    [Activity(Label = "GMapsDemo", MainLauncher = true)]
+    public class MainActivity : Activity, IOnMapReadyCallback
+    {
+        private GoogleMap _map;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Set up map
+            FragmentManager.FindFragmentById<MapFragment>(Resource.Id.mapFragment).GetMapAsync(this);
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            if (googleMap != null)
+            {
+                _map = googleMap;
+            }
+        }
+    }
+}
+
